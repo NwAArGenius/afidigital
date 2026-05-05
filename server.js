f7b22e9b7457cc4d2d9e0fb6822bedc992bcf0a0
NwAArGenius <aliouciss746@gmail.com>
2026-05-05 16:06:10 +0000

    docker-compose

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const seedFormations = require('./config/seed');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
